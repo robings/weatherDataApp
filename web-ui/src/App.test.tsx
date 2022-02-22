2fd449a149572effac50c0ee1c850ee5f09206ac
robings <58939401+robings@users.noreply.github.com>
2022-02-22 19:34:57 +0000

    Fix failing test.

--- web-ui/src/App.test.tsx
+++ web-ui/src/App.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
 test("renders weather forecast title", () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   expect(
     screen.getByRole("heading", { name: "Weather Forecast" })
   ).toBeInTheDocument();
